319929b3476f17f0859ab91d614f284305c2f2f3
Pauline Leininger <pauline.leininger@gmail.com>
2021-10-19 18:02:21 +0200

    Agent Field of Vision

---
+++ Assets/Scripts/Editor/AgentFieldOfViewEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AgentFieldOfView))]
+public class AgentFieldOfViewEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        AgentFieldOfView fov = (AgentFieldOfView)target;
+        Handles.color = Color.blue;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fov.visibleTargets)
+        {
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+}
